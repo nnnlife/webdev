a5df0cd53d47358ea1a27355bc11ae8c63c42a46
Yongwoo Park <yongwoo1.park@lge.com>
2018-07-15 17:41:50 +0900

    move directories because complicated directory structure
    move font and img directory to under dist directory because 
    express.static can expose one directory, did I think wrong?

--- conquer/backend/hello.js
+++ conquer/backend/hello.js
@@ -1,8 +1,33 @@
 var express = require('express');
 
 var app = express();
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+
+console.log('dir: ' + __dirname + '/../dist');
+
+
+app.use(express.static(__dirname + '/../dist'));
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/headers', function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  var s = '';
+  for(var name in req.headers) s += name + ": " + req.header[name] + '\n';
+  res.send(s);
+});
+
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' &&
+                          req.query.test === '1';
+  next();
+});
 
 app.use(function(req, res) {
   res.type('text/plain');
@@ -16,6 +41,8 @@ app.use(function(err, req, res, next) {
   res.status(500);
   res.send('500 - Server Error');
 });
+
+
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
